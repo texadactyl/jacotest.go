aa8946257eeef56b4ad08a0ae4ac97d74c49a406
Richard Elkins <richard.elkins@gmail.com>
2023-04-10 16:38:20 -0500

    showHelp will os.exit(0)

--- src/main.go
+++ src/main.go
@@ -19,8 +19,8 @@ func showHelp() {
     fmt.Printf("\t-j : This is the JVM to use in executing all test cases.  Default: java\n")
     fmt.Printf("\t-t : This is the timeout value in seconds (deadline) in executing all test cases.  Default: 60\n")
     fmt.Printf("\t-c : Clean all of the .class files and .log files\n\n")
-	ShowExecInfo()
-	os.Exit(1)
+    ShowExecInfo()
+    os.Exit(0)
 }
 
 //
