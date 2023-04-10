8d5798907fae39ad94e8f7f7007802147b1ddbdc
Richard Elkins <richard.elkins@gmail.com>
2023-04-10 08:42:57 -0500

    Modify JACOBIN-0227-string-array to avoid INVOKEDYNAMIC issues

--- tests/JACOBIN-0227-string-array/main.java
+++ tests/JACOBIN-0227-string-array/main.java
@@ -12,18 +12,20 @@ public class main {
         arr_ints[2] = 2;
         System.out.println("Just set all of the int array elements");
         int ndx;
-        String str;
         String[] arr_strings = new String[3];
         int size2 = arr_strings.length;
+
         System.out.print("arr_strings length: ");
         System.out.println(size2);
         arr_strings[0] = "Mary had a little lamb";
         System.out.println("Just set String array element 0");
         arr_strings[1] = "whose fleece was white as snow";
         arr_strings[2] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
+        System.out.println("Just set String array elements 1 and 2");
         for(ndx = 0; ndx < size2; ++ndx) {
-            str = ndx + ": " + arr_strings[ndx];
-            System.out.println(str);
+            System.out.print(ndx);
+            System.out.print(": ");
+            System.out.println(arr_strings[ndx]);
         }
         System.out.println("End");
     }
