668739de671801e55e0f29c38cff0ef7044c2dd0
Richard Elkins <richard.elkins@gmail.com>
2023-04-10 17:37:10 -0500

    Modified specrel to avoid INVODEDYNAMIC

--- tests/specrel/main.java
+++ tests/specrel/main.java
@@ -8,7 +8,10 @@ public class main
 	    double v_factorUpperBound = 0.91;
 	    double t_deltaAtRest = 10.0; // s
 	    double x_lengthAtRest = 42.0; // m
-	    System.out.println("===== Begin, t_deltaAtRest: " + String.valueOf(t_deltaAtRest) + ", x_lengthAtRest: " + String.valueOf(x_lengthAtRest));
+	    System.out.print("===== Begin, t_deltaAtRest: ");
+	    System.out.print(t_deltaAtRest);
+	    System.out.print(", x_lengthAtRest: ");
+	    System.out.println(x_lengthAtRest);
 	    Formulae lib = new Formulae();
 	    for(double v_factor = v_factorInit; v_factor < v_factorUpperBound; v_factor += v_factorDelta)
 	    {
@@ -16,8 +19,16 @@ public class main
 	        double lorry = lib.lorentzFactor(velocity);
 	        double stretched = lib.timeDilation(t_deltaAtRest, lorry);
 	        double shrunk = lib.lengthContraction(x_lengthAtRest, lorry);
-	        System.out.println("v: " + String.valueOf(v_factor) + " * c, Time(s): " + String.valueOf(t_deltaAtRest) + " --> " + String.valueOf(stretched));
-	        System.out.println("\tLength(m): " + String.valueOf(x_lengthAtRest) + " --> " + String.valueOf(shrunk));
+	        System.out.print("v: ");
+	        System.out.print(v_factor);
+	        System.out.print(" * c, Time(s): ");
+	        System.out.print(t_deltaAtRest);
+	        System.out.print(" --> ");
+	        System.out.println(stretched);
+	        System.out.print("\tLength(m): ");
+	        System.out.print(x_lengthAtRest);
+	        System.out.print(" --> ");
+	        System.out.println(shrunk);
 	    }
 	    System.out.println("===== End");
     }
