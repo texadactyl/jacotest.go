5198c5ad219bdc5796ccc3f76f211beb48188ee0
Richard Elkins <richard.elkins@gmail.com>
2023-04-18 11:05:19 -0500

    avoid workflow seeing a Java exception in  negtest-runner-throw-exception

--- tests/negtest-runner-throw-exception/main.java
+++ tests/negtest-runner-throw-exception/main.java
@@ -1,9 +1,15 @@
 public class main {
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) throws NumberFormatException {
         
-        System.out.println("I will throw an Exception!");
-        throw new Exception("Exception thrown");
-
+        System.out.println("I will catch a NumberFormatException");
+        try {
+        	int ii = Integer.parseInt("ABC");
+        } catch(NumberFormatException ex) {
+        	System.out.println("Caught NumberFormatException");
+        	System.exit(0);
+        }
+        System.out.println("*** FAILED to catch NumberFormatException");
+		System.exit(1);
     }
 }
