d68422db6bdfd976f3ba10e7d74e74a1f5c82ea9
Richard Elkins <richard.elkins@gmail.com>
2023-04-22 14:58:21 -0500

    Keep executables in $HOME/go/bin per advice from golang

--- builder.sh
+++ builder.sh
@@ -4,13 +4,13 @@ source ./periodic/common_defs.txt
 > $LOG # Make the log nil.
 logbegin
 
-logger 'Build jacotest .....'
+logger 'Build and install jacotest .....'
 cd src
 if [ $? -ne 0 ]; then
     oops 'FAILED: cd src'
 fi
 go clean -cache
-go build -o $JACOBIN_BIN/jacotest 2>&1 | tee -a $LOG
+go install 2>&1 | tee -a $LOG
 if [ $? -ne 0 ]; then
     oops 'FAILED: go build'
 fi
