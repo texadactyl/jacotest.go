8ba47d0036cde3ef7ec25ef8578c8a4d4e263837
Richard Elkins <richard.elkins@gmail.com>
2023-05-24 03:20:46 -0500

    add @echo off

--- windows/builder.bat
+++ windows/builder.bat
@@ -1,3 +1,4 @@
+@echo off
 set "GOPATH=%USERPROFILE%\go" || EXIT /B 1
 set "GOBIN=%GOPATH%\bin" || EXIT /B 1
 setx PATH "%GOBIN%";"%PATH%" || EXIT /B 1
@@ -12,4 +13,3 @@ go vet || EXIT /B 1
 
 cd ..  || EXIT /B 1
 jacotest -h || EXIT /B 1
-
