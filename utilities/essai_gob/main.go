8fb3fd5c3a2f387914dcdbf8d7b13ac22440dbb4
Richard Elkins <richard.elkins@gmail.com>
2023-06-04 11:00:48 -0500

    add essai_gob to utilities

---
+++ utilities/essai_gob/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"os"
+	"encoding/gob"
+	"fmt"
+	"log"
+)
+
+const GobFile = "class_prefix_map.gob"
+
+func main() {
+
+	// Build input map
+	m1 := make(map[string]string)
+	m1["com/sun/crypto/provider/AESCipher$AES128_CBC_NoPadding.class"] = "java.base.jmod"
+	m1["com/sun/crypto/provider/AESCipher$AES128_CFB_NoPadding.class"] = "java.base.jmod"
+	m1["com/sun/crypto/provider/AESCipher$AES128_ECB_NoPadding.class"] = "java.base.jmod"
+
+	// Open output file
+	outFile, err := os.Create(GobFile)
+	if err != nil {
+		log.Fatal("encoding os.Create:", err)
+	}
+    inky := gob.NewEncoder(outFile)
+	
+	// Create an encoder and send a value.
+	err = inky.Encode(&m1)
+	if err != nil {
+		log.Fatal("gob Encode:", err)
+	}
+	
+	// Close the output file
+	err = outFile.Close()
+	if err != nil {
+		log.Fatal("encoding os.Close:", err)
+	}
+
+	// Initialise input map
+	m2 := make(map[string]string)
+	
+	// Open input file
+	inFile, err := os.Open(GobFile)
+	if err != nil {
+		log.Fatal("encoding os.Create:", err)
+	}
+
+	// Create a decoder and receive a value.
+	dinky := gob.NewDecoder(inFile)
+	err = dinky.Decode(&m2)
+	if err != nil {
+		log.Fatal("gob Decode:", err)
+	}
+
+	// Show the 2 maps	
+	fmt.Println("m1 = ", m1)
+	fmt.Println("m2 = ", m2)
+	
+	// Success?
+	if len(m1) != len(m2) {
+		log.Fatal("len(m1) != len(m2)") // Sadly, no.
+	}
+	if m1["com/sun/crypto/provider/AESCipher$AES128_CBC_NoPadding.class"] != m2["com/sun/crypto/provider/AESCipher$AES128_CBC_NoPadding.class"] {
+		log.Fatal("m1 != m2") // Sadly, no.
+	}
+
+	// Success!
+	fmt.Println("End")
+	
+}
+
