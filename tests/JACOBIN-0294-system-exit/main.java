555d5a1c97caf5885b306005ad6c1abe2d1fa4f9
Richard Elkins <richard.elkins@gmail.com>
2023-06-25 15:56:47 -0500

    JACOBIN-0294-system-exit/main.java - windows fixup

--- tests/JACOBIN-0294-system-exit/main.java
+++ tests/JACOBIN-0294-system-exit/main.java
@@ -1,12 +1,16 @@
 import java.io.*;
 public class main {
 
-    public static int cmd(String text) {
-        System.out.print("cmd: Begin, text=");
-        System.out.println(text);
+    public static int commander(String cmd, String dirstr) {
+        System.out.print("commander: Begin, cmd=");
+        System.out.print(cmd);
+        System.out.print(", dirstr=");
+        System.out.println(dirstr);
         try {
             String line;
-            Process process = Runtime.getRuntime().exec(text);
+            String [] nullstr = {};
+            File dir = new File(dirstr);
+            Process process = Runtime.getRuntime().exec(cmd, nullstr, dir);
             BufferedReader bri = new BufferedReader
                     (new InputStreamReader(process.getInputStream()));
             BufferedReader bre = new BufferedReader
@@ -20,52 +24,58 @@ public class main {
             }
             bre.close();
             int statusCode = process.waitFor();
-            System.out.print("cmd: End, status code = ");
+            System.out.print("commander: End, status code = ");
             System.out.println(statusCode);
             return statusCode;
         }
         catch (Exception err) {
-            System.out.print("cmd: ");
+            System.out.print("commander: ");
             System.out.println(err);
             err.printStackTrace();
+        	return 86;
         }
-        return 86;
     }
 
     public static void main(String args[]) {
 
-    	ProcessHandle ph = ProcessHandle.current();
-		ProcessHandle.Info info = ph.info();
-		String jvmPath = info.command().orElse("?");
-		if (jvmPath.equals("?")) {
-			System.out.println("*** ERROR, ProcessHandle.Info.command() failed.");
-			System.exit(1);
-		}
-        System.out.print("JVM executable: ");
-        System.out.println(jvmPath);
-        File file = new File(jvmPath);
-        String jvmName = file.getName();
-        System.out.print("Argument count: ");
-        System.out.println(args.length);
 		if (args.length == 0) {
-		    System.out.println("I am the PARENT process.");
+		    System.out.print("parent: Argument count: ");
+		    System.out.println(args.length);
+			String cwd = System.getProperty("user.dir");
+		    System.out.print("parent: I am here: ");
+		    System.out.println(cwd);
+			ProcessHandle ph = ProcessHandle.current();
+			ProcessHandle.Info info = ph.info();
+			String jvmPath = info.command().orElse("?");
+			if (jvmPath.equals("?")) {
+				System.out.println("*** ERROR, parent: ProcessHandle.Info.command() failed.");
+				System.exit(1);
+			}
+		    System.out.print("parent: JVM executable: ");
+		    System.out.println(jvmPath);
+		    File file = new File(jvmPath);
+		    String jvmName = file.getName();
 		    String text;
 		    if (jvmName.equals("java"))
 		    	text = jvmPath.concat(" main 123"); // OpenJDK
 		    else
 		    	text = jvmPath.concat(" main.class 123"); // jacobin
-		    int statusCode = cmd(text);
+		    int statusCode = commander(text, cwd);
 		    if (statusCode == 42) {
-		    	System.out.println("The subprocess exited with 42 as expected.");
+		    	System.out.println("parent: The child process exited with 42 as expected.");
 		    	System.exit(0);
 		    } else {
-		    	System.out.print("*** ERROR, expected 42 but the subprocess exited with status ");
+		    	System.out.print("*** ERROR, parent: expected 42 but the child process exited with status ");
 		    	System.out.println(statusCode);
 		    	System.exit(1);
 		    }
 		} else {
-		    System.out.println("I am the CHILD process.");
-			System.out.println("Next, I will System.exit(42).");
+		    System.out.print("child: Argument count: ");
+		    System.out.println(args.length);
+			String cwd = System.getProperty("user.dir");
+		    System.out.print("child: I am here: ");
+		    System.out.println(cwd);
+			System.out.println("child: Next, I will System.exit(42).");
 		    System.exit(42);
 		}
 
