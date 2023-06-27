4e93890c95b91e261aeba7ca5481da2d5f9fcef3
Richard Elkins <richard.elkins@gmail.com>
2023-06-27 13:20:09 -0500

    small edits

--- tests/JACOBIN-0303-securerandom/main.java
+++ tests/JACOBIN-0303-securerandom/main.java
@@ -1,7 +1,6 @@
 public class main {
     public static void main(String args[]) {
-		System.out.println("Investigate the following:");
-		System.out.println("Found class: java/security/SecureRandom, but it did not contain method: nextLong");
+		System.out.println("Investigate missing nextLong as reported in JACOBIN-303");
 		int a = (int) (Math.random() * 256);
 		System.out.println("Cannot reproduce!");
     }
