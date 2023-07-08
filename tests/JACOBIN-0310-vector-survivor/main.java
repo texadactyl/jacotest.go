1f4338a01cb0bc50f0cb266c2d3e1a8c7af173dc
Richard Elkins <richard.elkins@gmail.com>
2023-07-08 10:39:12 -0500

    new test: JACOBIN-0310-vector-survivor

---
+++ tests/JACOBIN-0310-vector-survivor/main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Vector;
+public class main {
+    public static void main(String[] args) {
+        Integer[] vintIntegers = {1, 2, 3, 4, 5, 6};
+        Vector<Integer> vint = new Vector<Integer>(Arrays.asList(vintIntegers));
+        System.out.println("I survived!");
+   }
+}
+
