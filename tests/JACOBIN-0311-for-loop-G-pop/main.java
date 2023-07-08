4273fdfec24d380f4458510ed5cc753692d49dba
Richard Elkins <richard.elkins@gmail.com>
2023-07-08 17:00:02 -0500

    new test case: JACOBIN-0311-for-loop-G-pop

---
+++ tests/JACOBIN-0311-for-loop-G-pop/main.java
@@ -0,0 +1,14 @@
+public class main {
+	private static final int NLOOPS = 10;
+	private static final double Pi = 3.14159265;
+	public static void main(String args[]) {    
+    	double xx, yy;
+    	for (int ii = 0; ii < NLOOPS; ii++) {
+    		xx = Math.random() * Pi;
+    		yy = Math.cos(xx);
+    		System.out.print(ii);
+			System.out.print(") ");
+			System.out.println(xx);
+		}
+    }
+}
