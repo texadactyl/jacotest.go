d6d6e27b910159a44f6d232d0535d19ab1fad941
Richard Elkins <richard.elkins@gmail.com>
2023-07-21 12:51:06 -0500

    fix JACOBIN-0322-throw-null

--- tests/JACOBIN-0322-throw-null/main.java
+++ tests/JACOBIN-0322-throw-null/main.java
@@ -1,5 +1,13 @@
 public class main {
 	public static void main(String[] args){
-        throw new NullPointerException("Why not?!");
+        System.out.println("I will catch a NullPointerException");
+        try {
+            throw new NullPointerException("Can you see my null pointer exception?");
+        } catch (NullPointerException ex) {
+            System.out.println("Caught NullPointerException");
+            System.exit(0);
+        }
+        System.out.println("*** ERROR, failed to catch NullPointerException");
+        System.exit(1);
     }
 }
