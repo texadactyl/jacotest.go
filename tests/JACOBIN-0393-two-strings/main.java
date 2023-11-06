1e5debfb53db2579df76dcd2a6f15ae27f920e0e
Richard Elkins <richard.elkins@gmail.com>
2023-11-06 13:44:14 -0600

    enhance tests/JACOBIN-0393-two-string

--- tests/JACOBIN-0393-two-strings/main.java
+++ tests/JACOBIN-0393-two-strings/main.java
@@ -1,19 +1,20 @@
-import java.util.Map;
-import java.util.HashMap;
-
 public class main {
 	public static void main(String[] args) {
 		double dd = 42.0d;
-		String fmtPrintf = "================= 1a. printf(\"This is a String variable format string\") --> %s %s";
-		String fmtFormat = "================= 2. String.format --> %s %s";
-		String fmtFormatter = "================= 3. format.formatter --> %s %s";
+		String fmtPrintf_1 = "================= printf(\"This is a String variable format string\") --> %s %s";
+		String fmtPrintf_2 = "================= printf(\"This is a String variable format string without any objects to printf";
+		String fmtFormat = "================= String.format --> %s %s";
+		String fmtFormatter = "================= format.formatter --> %s %s";
 		String s1 = "ABC";
 		String s2 = "DEF";
 
-		System.out.printf(fmtPrintf, s1, s2);
+		System.out.printf(fmtPrintf_1, s1, s2);
 		System.out.println("\n");
 		
-		System.out.printf("================= 1b. printf(\"This is a literal format string\") --> %s %s", s1, s2);
+		System.out.printf(fmtPrintf_2);
+		System.out.println("\n");
+		
+		System.out.printf("================= printf(\"This is a literal format string\") --> %s %s", s1, s2);
 		System.out.println("\n");
 
 		String s3 = String.format(fmtFormat, s1, s2);
@@ -24,7 +25,11 @@ public class main {
 		System.out.print(s3);
 		System.out.println("\n");
 		
-		System.out.print("================= 4. print(\"This is a literal\")");
+		System.out.print("================= print(\"This is a literal\")");
+		System.out.println("\n");
+		
+		String s4 = "================= This String is s4";
+		System.out.print(s4);
 		System.out.println("\n");
 	}
 }
