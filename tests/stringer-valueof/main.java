c9f8a3af1c489a1a337f88821a4666844ef95aad
Richard Elkins <richard.elkins@gmail.com>
2023-11-10 16:53:15 -0600

    fixed and expanded test case stringer-valueof

--- tests/stringer-valueof/main.java
+++ tests/stringer-valueof/main.java
@@ -1,46 +1,68 @@
 public class main {
 
-    public static void printLabeledString(String label, String value) {
-        System.out.print(label);
-        System.out.println(value);
-    }
-
-    public static int isItTrue(String label, boolean bool) {
-        if (bool) {
-            printLabeledString("Success :: ", label);
+    public static int isItTrue(String label, String expected, String observed) {
+        if (expected.equals(observed)) {
+            System.out.printf("Success :: %s=%s\n", label, expected);
             return 0;
         }
-        printLabeledString("*** ERROR, ", label);
+        System.out.printf("*** ERROR, %s expected=%s, observed=%s\n", label, expected, observed);
         return 1;
     }
     
     public static void main(String[] args) {
     
     	int errorCount = 0;
+		String beetlejuice = "Beetlejuice";
+		byte bb = 0x42; // "66"
+		char cc = 'A';
+		char [] ca = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+		double dd = 3.0d;
+		float ff = 5.0f;
+		int ii = 7;
+		long jj = 11;
+		short ss = 13;
+		boolean zz = true;
     
     	System.out.println("Set a String array element to various String.valueOf arguments.");
-        String[][] s2d = new String[3][4];
-        System.out.println("String.valueOf(42) .....");
-        s2d[0][0] = String.valueOf(42);
-        errorCount += isItTrue("42 --> \"42\"", s2d[0][0].equals("42"));
-        System.out.println("String.valueOf(42L) .....");
-        s2d[0][0] = String.valueOf(42L);
-        errorCount += isItTrue("42L --> \"42\"", s2d[0][0].equals("42"));
-        System.out.println("String.valueOf(4.2F) .....");
-        s2d[0][0] = String.valueOf(4.2F);
-        errorCount += isItTrue("4.2F --> \"4.2\"", s2d[0][0].equals("4.2"));
-        System.out.println("String.valueOf(4.2D) .....");
-        s2d[0][0] = String.valueOf(4.2D);
-        errorCount += isItTrue("4.2D --> \"4.2\"", s2d[0][0].equals("4.2"));
-        System.out.println("String.valueOf(\"42\") .....");
-        s2d[0][0] = String.valueOf("42");
-        errorCount += isItTrue("\"42\" --> \"42\"", s2d[0][0].equals("42"));
-        System.out.println("String.valueOf('A') .....");
-        s2d[0][0] = String.valueOf('A');
-        errorCount += isItTrue("'A' --> \"A\"", s2d[0][0].equals("A"));
+    	String str;
+    	
+        str = String.valueOf(bb);
+        errorCount += isItTrue("bb", "66", str);
+        
+        str = String.valueOf(cc);
+        errorCount += isItTrue("cc", "A", str);
+        
+        str = String.valueOf(ca);
+        errorCount += isItTrue("ca", "ABCDEFGH", str);
+        
+        str = String.valueOf(ca, 2, 3);
+        errorCount += isItTrue("ca subarray", "CDE", str);
+        
+        str = String.valueOf(dd);
+        errorCount += isItTrue("dd", "3.0", str);
+        
+        str = String.valueOf(ff);
+        errorCount += isItTrue("ff", "5.0", str);
+        
+        str = String.valueOf(ii);
+        errorCount += isItTrue("ii", "7", str);
+        
+        str = String.valueOf(jj);
+        errorCount += isItTrue("jj", "11", str);
+        
+        str = String.valueOf(ss);
+        errorCount += isItTrue("ss", "13", str);
+        
+        str = String.valueOf(zz);
+        errorCount += isItTrue("zz true", "true", str);
+        
+		zz = false;
+        str = String.valueOf(zz);
+        errorCount += isItTrue("zz false", "false", str);
+        
     	System.out.println("Did not crash!");
-
         assert (errorCount == 0);
+		System.out.printf("Error count = %d\n", errorCount);
 
     }
     
