ce3c72d7af53723c1612e70a80d48585e1b4bef7
Richard Elkins <richard.elkins@gmail.com>
2023-11-23 15:50:50 -0600

    Update MyFormatter.java - again, because of windows

--- tests/java-logging/MyFormatter.java
+++ tests/java-logging/MyFormatter.java
@@ -1,4 +1,4 @@
-()import java.util.Date;
+import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
@@ -6,13 +6,14 @@ public class MyFormatter extends Formatter {
 
     @Override
     public String format(LogRecord record) {
-        return
-        	record.getLevel().toString()
-        		+ " :: " + record.getSequenceNumber​()
-               	+ " :: " + record.getSourceClassName()
-               	+ "." + record.getSourceMethodName()
-               	+ " :: " + new Date(record.getMillis())
-               	+ String.format(" :: %s\n", record.getMessage());
+        String result = String.format("%s :: %03d :: %s.%s :: %s :: %s\n", 
+        		record.getLevel().toString(),
+        		record.getSequenceNumber​(),
+               	record.getSourceClassName(),
+               	record.getSourceMethodName(),
+               	new Date(record.getMillis()),
+               	record.getMessage() );
+        return result;
     }
 
 }
