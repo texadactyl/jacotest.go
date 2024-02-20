ac51f7f4c3f00af671d62bd8818f4400074e45e3
Richard Elkins <richard.elkins@gmail.com>
2024-02-20 10:22:11 -0600

    tests/JACOBIN-0435-format-neg-int-as-hex

--- tests/JACOBIN-0435-format-neg-int-as-hex/main.java
+++ tests/JACOBIN-0435-format-neg-int-as-hex/main.java
@@ -8,12 +8,16 @@ public class main {
                 (byte)(arg),
         };
         String str = String.format("%02x%02x%02x%02x", bb[0], bb[1], bb[2], bb[3]);
-        System.out.println(str);
         return str;
     }
     public static void main(String[] args) {
         int neg64 = -64;
-        String str = printIntInHex(neg64);
-        assert (str.equals("ffffffc0"));
+        String expValue = new String("ffffffc0");
+        String obsValue = printIntInHex(neg64);
+        System.out.print("Expected value: ");
+        System.out.println(expValue);
+        System.out.print("Observed value: ");
+        System.out.println(obsValue);
+        assert (obsValue.equals(expValue));
     }
 }  
