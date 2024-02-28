a6c2892cb680b926a28a173f58cd4d0003310396
Richard Elkins <richard.elkins@gmail.com>
2024-02-28 12:18:41 -0600

    remove char types from JACOBIN-0393-pot-pourri

--- tests/JACOBIN-0393-pot-pourri/main.java
+++ tests/JACOBIN-0393-pot-pourri/main.java
@@ -4,12 +4,11 @@ public class main {
 	
 		boolean tryFormatting = true;
 	
-		String fmtString = "All together now! %% String:%s byte:0x%02X char:%c double:'%e' float:'%5.2f' int:%d long:%d short:%d\n";
+		String fmtString = "All together now! %% String:%s byte:0x%02X double:'%e' float:'%5.2f' int:%d long:%d short:%d\n";
 		String fmt = "%f\n";
 
 		String beetlejuice = "Beetlejuice";
 		byte bb = 0x02;
-		char cc = 'A';
 		double dd = 3.0d;
 		float ff = 5.0f;
 		int ii = 7;
@@ -20,12 +19,12 @@ public class main {
 		
 		if (tryFormatting) {
 			System.out.printf("Hello!\n");
-			System.out.printf(fmtString, beetlejuice, bb, cc, dd, ff, ii, jj, ss);
+			System.out.printf(fmtString, beetlejuice, bb, dd, ff, ii, jj, ss);
 
-			string = String.format(fmtString, beetlejuice, bb, cc, dd, ff, ii, jj, ss);
+			string = String.format(fmtString, beetlejuice, bb, dd, ff, ii, jj, ss);
 			System.out.print(string);
 
-			string = fmtString.formatted(beetlejuice, bb, cc, dd, ff, ii, jj, ss);
+			string = fmtString.formatted(beetlejuice, bb, dd, ff, ii, jj, ss);
 			System.out.print(string);
 		}		
 	}
