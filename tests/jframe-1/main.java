20cfa2f079c9712e909b56dd2112c23aabeca978
Richard Elkins <richard.elkins@gmail.com>
2024-03-19 13:40:53 -0500

    new test case: jframe-1

---
+++ tests/jframe-1/main.java
@@ -0,0 +1,25 @@
+// hacked from https://docs.oracle.com/javase%2Ftutorial%2Fuiswing%2F%2F/examples/components/SimpleTableDemoProject/src/components/SimpleTableDemo.java
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class main {
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        SimpleTableDemo std = new SimpleTableDemo();
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	while (! std.finished) {
+                	std.createAndProcJframe();
+                }
+                assert (std.errCount == 0);
+                	
+                System.exit(0);
+            }
+        });
+    }
+    
+}
