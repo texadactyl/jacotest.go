7bdc44ff8e2e3e02febd8e7b75e88f6493bd9272
Richard Elkins <richard.elkins@gmail.com>
2024-05-05 14:33:58 -0500

    new test case: random-1

--- HISTORY.md
+++ HISTORY.md
@@ -4,6 +4,7 @@ This file is a version history of jacotest amendments.  Entries appear in versio
 | `Date` | `Version` | `Contents` |
 | :------------: | :---: | :--- |
 |<img width=90/>|<img width=60/>|<img width=600/>|
+| 2024-05-05 | 3.2.3 | New test case: random-1. |
 | 2024-05-04 | 3.2.2 | New test cases: natives-double and natives-float. |
 | 2024-05-03 | 3.2.1 | Implement compiler option -Werror (treat warnings as errors). |
 | 2024-04-27 | 3.1.14 | Updated test case: charset-encoding. |
---
+++ tests/random-1/main.java
@@ -0,0 +1,94 @@
+import java.util.Random;
+
+public class main {
+	public static void main(String[] args) {
+	
+		int LOOPS = 10;
+		int BOUND = 20;
+		int BBSIZE = 8;
+		long MYSEED = 42;
+		int errorCount = 0;
+		
+		byte[] bb = new byte[BBSIZE];
+		
+		boolean abool;
+		int ai, aib;
+		long aj, ajb;
+		float af;
+		double ad;
+		double ag;
+		
+		int errorCounter = 0;
+	
+		Random rr = new Random();
+		for (int xx = 0; xx < LOOPS; xx++) {
+			abool = rr.nextBoolean();
+			ai = rr.nextInt();
+			aib = rr.nextInt(BOUND);
+			if (aib < 0 || aib >= BOUND) {
+				System.out.printf("*** ERROR, integer out of bounds (%d)\n", aib);
+				++errorCount;
+			}
+			aj = rr.nextLong();
+			ajb = rr.nextLong(BOUND);
+			if (ajb < 0 || ajb >= BOUND) {
+				System.out.printf("*** ERROR, long out of bounds (%d)\n", ajb);
+				++errorCount;
+			}
+			af = rr.nextFloat();
+			if (af < 0.0f || af >= 1.0f) {
+				System.out.printf("*** ERROR, float out of bounds (%d)\n", af);
+				++errorCount;
+			}
+			ad = rr.nextDouble();
+			if (ad < 0.0 || ad >= 1.0) {
+				System.out.printf("*** ERROR, double out of bounds (%d)\n", ad);
+				++errorCount;
+			}
+			ag = rr.nextGaussian();
+			rr.nextBytes(bb);
+			System.out.print(abool);
+			System.out.printf(" %d %d %d %f %f %f; bb:", ai, aib, aj, af, ad, ag);
+			for (int yy = 0; yy < BBSIZE; yy++) {
+				System.out.printf(" %02x", bb[yy]);
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\n==========================================\n");
+	
+		rr = new Random(MYSEED);
+		for (int xx = 0; xx < LOOPS; xx++) {
+			abool = rr.nextBoolean();
+			ai = rr.nextInt();
+			aib = rr.nextInt(BOUND);
+			if (aib < 0 || aib >= BOUND) {
+				System.out.printf("*** ERROR, integer out of bounds (%d)\n", aib);
+				++errorCount;
+			}
+			aj = rr.nextLong();
+			ajb = rr.nextLong(BOUND);
+			if (ajb < 0 || ajb >= BOUND) {
+				System.out.printf("*** ERROR, long out of bounds (%d)\n", ajb);
+				++errorCount;
+			}
+			af = rr.nextFloat();
+			ad = rr.nextDouble();
+			if (ad < 0.0 || ad >= 1.0) {
+				System.out.printf("*** ERROR, double out of bounds (%d)\n", ad);
+				++errorCount;
+			}
+			ag = rr.nextGaussian();
+			rr.nextBytes(bb);
+			System.out.print(abool);
+			System.out.printf(" %d %d %d %f %f %f; bb:", ai, aib, aj, af, ad, ag);
+			for (int yy = 0; yy < BBSIZE; yy++) {
+				System.out.printf(" %02x", bb[yy]);
+			}
+			System.out.println();
+		}
+		
+		assert(errorCount == 0);
+	
+	}
+}
