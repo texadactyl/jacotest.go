0209350f53c0f82c1265d633af3cd3cacea1a0e1
Richard Elkins <richard.elkins@gmail.com>
2024-08-19 12:37:53 -0500

    New test case: catch-8-survivor

---
+++ tests/catch-8-survivor/main.java
@@ -0,0 +1,66 @@
+public class main {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        int iparsed;
+        String fmt = "[%d] Caught NumberFormatException\n";
+        String errMsg;
+        
+	    try {
+            iparsed = Integer.parseInt("ABC");
+            errMsg = String.format("[%d] Failed to catch NumberFormatException", ++counter);
+            throw new AssertionError(errMsg);
+		} catch (NumberFormatException ex1) {
+			System.out.printf(fmt, ++counter);
+	        try {
+                iparsed = Integer.parseInt("ABC");
+                errMsg = String.format("[%d] Failed to catch NumberFormatException", ++counter);
+                throw new AssertionError(errMsg);
+		    } catch (NumberFormatException ex2) {
+			    System.out.printf(fmt, ++counter);
+                try {
+                    iparsed = Integer.parseInt("ABC");
+                    errMsg = String.format("[%d] Failed to catch NumberFormatException", ++counter);
+                    throw new AssertionError(errMsg);
+	            } catch (NumberFormatException ex3) {
+		            System.out.printf(fmt, ++counter);
+                    try {
+                        iparsed = Integer.parseInt("ABC");
+                        errMsg = String.format("[%d] Failed to catch NumberFormatException", ++counter);
+                        throw new AssertionError(errMsg);
+	                } catch (NumberFormatException ex4) {
+		                System.out.printf(fmt, ++counter);
+                        try {
+                            iparsed = Integer.parseInt("ABC");
+                            errMsg = String.format("[%d] Failed to catch NumberFormatException", ++counter);
+                            throw new AssertionError(errMsg);
+	                    } catch (NumberFormatException ex5) {
+		                    System.out.printf(fmt, ++counter);
+                            try {
+                                iparsed = Integer.parseInt("ABC");
+                                errMsg = String.format("[%d] Failed to catch NumberFormatException", ++counter);
+                                throw new AssertionError(errMsg);
+	                        } catch (NumberFormatException ex6) {
+		                        System.out.printf(fmt, ++counter);
+                                try {
+                                    iparsed = Integer.parseInt("ABC");
+                                    errMsg = String.format("[%d] Failed to catch NumberFormatException", ++counter);
+                                    throw new AssertionError(errMsg);
+	                            } catch (NumberFormatException ex7) {
+		                            System.out.printf(fmt, ++counter);
+                                    try {
+                                        iparsed = Integer.parseInt("ABC");
+                                        errMsg = String.format("[%d] Failed to catch NumberFormatException", ++counter);
+                                        throw new AssertionError(errMsg);
+	                                } catch (NumberFormatException ex8) {
+		                                System.out.printf(fmt, ++counter);
+	                                }
+	                            }
+	                        }
+	                    }
+	                }
+	            }
+		    }
+		}
+    }
+}
