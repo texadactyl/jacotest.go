d7cc9f2e8a74e70b76a99580031aa37cf25d548a
Richard Elkins <richard.elkins@gmail.com>
2024-09-22 15:16:42 -0500

    don't run Java 21 features on Java 17   (:

---
+++ tests/JACOBIN-0322-default-locale/main.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+public class main {
+
+	public static void main(String[] args)	{
+	    int NLOOPS = 10000;
+	    Locale locale1 = Locale.of("en");
+	    Locale locale2 = locale1;
+	    Locale locale3 = locale1;
+	    Locale locale4 = locale1;
+	    
+	    for (int ii = 0; ii < NLOOPS; ii++) {
+		    locale1 = Locale.getDefault();
+		    locale2 = Locale.of("English");
+		    locale3 = Locale.of("English", "USA");
+		    locale4 = Locale.of("English", "USA", "Texas");
+		}
+		
+		System.out.println(locale1);
+		System.out.println(locale2);
+		System.out.println(locale3);
+		System.out.println(locale4);
+	}
+}
