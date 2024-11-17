e00d0a4cbe5ef2e1fb9187a09cfcf87cbc7d37d0
Richard Elkins <richard.elkins@gmail.com>
2024-11-17 09:46:34 -0600

    fix indentation

--- tests/JACOBIN-0433-HexFormat/main.java
+++ tests/JACOBIN-0433-HexFormat/main.java
@@ -12,19 +12,19 @@ public class main {
 
     public static void main(String[] args) throws Exception {
         int errorCount = 0;
-		HexFormat hex = HexFormat.ofDelimiter(":");
-		System.out.println("HexFormat.ofDelimiter(:) instantiated ok");
-		System.out.println(hex.toString());
+	HexFormat hex = HexFormat.ofDelimiter(":");
+	System.out.println("HexFormat.ofDelimiter(:) instantiated ok");
+	System.out.println(hex.toString());
 
         int ii = 65537;
         short ss = (short) ii;
         errorCount += isItTrue("ii = 65537 and (ss <-- ii) == 1", ss == 1, String.valueOf(ss));
-		char cc = (char) ii;
-		String st = hex.toHexDigits(cc);
-		errorCount += isItTrue("ii = 65537 and (cc <-- ii) == 0x01", cc == 0x01, st);
-		byte bb = (byte) ii;
-		st = hex.toHexDigits(bb);
-		errorCount += isItTrue("ii = 65537 and (bb <-- ii) == 0x01", bb == 0x01, st);
+	char cc = (char) ii;
+	String st = hex.toHexDigits(cc);
+	errorCount += isItTrue("ii = 65537 and (cc <-- ii) == 0x01", cc == 0x01, st);
+	byte bb = (byte) ii;
+	st = hex.toHexDigits(bb);
+	errorCount += isItTrue("ii = 65537 and (bb <-- ii) == 0x01", bb == 0x01, st);
 
         // Check the error count
         assert (errorCount == 0);
