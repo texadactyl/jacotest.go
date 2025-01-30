7365060914a8b8fecb029e36a7aad768605e3802
Richard Elkins <richard.elkins@gmail.com>
2025-01-30 08:03:02 -0600

    make sure show_subclassing.sh output is sorted

--- show_subclassing.sh
+++ show_subclassing.sh
@@ -11,9 +11,10 @@ fi
 # 1. Find all occurences of main.java and grep all "class" statements, keeping the file paths (-H) and ignoring case (-i).
 # 2. From that, ignore case and throw out (-v) the "class main" statements.
 # 3. From that, remove the directory prefix, yielding only the jacotest case  (directory) name.
+# 4. From that, sort.
 #
-# 1 vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv         2 vvvvvvvvvvvvvvvvvvv        3 vvvvvvvvvvvvvvvvvvvvvv
+# 1 vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv         2 vvvvvvvvvvvvvvvvvvv        3 vvvvvvvvvvvvvvvvvvvvvv   4 vv
 echo
 echo $MYNAME': Jacotest case files exhibiting subordinate classes (duplicate entries are possible):' 2>&1  | tee $MYNAME.log
 echo 2>&1  | tee -a $MYNAME.log
-find ./tests -name main.java -exec grep -iH " class " {} \;  2>&1 | grep -iv "class main" 2>&1 | sed -e "s/\.\/tests\///" 2>&1  | tee -a $MYNAME.log
+find ./tests -name main.java -exec grep -iH " class " {} \;  2>&1 | grep -iv "class main" 2>&1 | sed -e "s/\.\/tests\///" | sort 2>&1  | tee -a $MYNAME.log
