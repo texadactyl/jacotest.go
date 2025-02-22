daeb34af9368a0848f7fb308de27722b7135676e
Richard Elkins <richard.elkins@gmail.com>
2025-02-22 16:34:31 -0600

    expand final-static

--- tests/final-static/main.java
+++ tests/final-static/main.java
@@ -1,17 +1,23 @@
 public class main {
 
     private static byte b1 = 0x41;
-    private static final byte b2 = 0x42;
+    private final static byte b2 = 0x42;
+    private static byte ba1[] = {0x41, 0x42};
+    private final static byte[] ba2 = {0x43, 0x44};
     private static char c1 = 'A';
-    private static final char c2 = 'B';
+    private final static char c2 = 'B';
     private static float f1 = 1.0f;
-    private static final float f2 = 2.0f;
+    private final static float f2 = 2.0f;
     private static double d1 = 1.0;
-    private static final double d2 = 2.0;
+    private final static double d2 = 2.0;
     private static int i1 = 1;
-    private static final int i2 = 2;
+    private final static int i2 = 2;
     private static long j1 = 1;
-    private static final long j2 = 2;
+    private final static long j2 = 2;
+    private static short s1 = 1;
+    private final static short s2 = 2;
+    private static String str1 = "1";
+    private final static String str2 = "2";
 
     // Check observed string to expected string.
     private static int checker(String label, String expected, String observed) {
@@ -20,7 +26,7 @@ public class main {
             return 0;
         }
         if (observed.equals(expected)) {
-            System.out.printf("Success :: %s\n", label);
+            System.out.printf("Success :: %s = %s\n", label, observed);
             return 0;
         }
         System.out.printf("*** ERROR :: %s, expected=\"%s\", observed=\"%s\"\n", label, expected, observed);
@@ -30,6 +36,8 @@ public class main {
     public static void main(String[] args) {
         int errorCount = 0;
 
+        errorCount += checker("static main.b1", "0x41", jj._getStaticString("main", "b1"));
+        errorCount += checker("static main.b2", "0x42", jj._getStaticString("main", "b2"));
         errorCount += checker("static main.c1", "A", jj._getStaticString("main", "c1"));
         errorCount += checker("static main.c2", "B", jj._getStaticString("main", "c2"));
         errorCount += checker("static main.d1", "1", jj._getStaticString("main", "d1"));
@@ -40,6 +48,10 @@ public class main {
         errorCount += checker("static main.i2", "2", jj._getStaticString("main", "i2"));
         errorCount += checker("static main.j1", "1", jj._getStaticString("main", "j1"));
         errorCount += checker("static main.j2", "2", jj._getStaticString("main", "j2"));
+        errorCount += checker("static main.s1", "1", jj._getStaticString("main", "s1"));
+        errorCount += checker("static main.s2", "2", jj._getStaticString("main", "s2"));
+        errorCount += checker("static main.str1", "1", jj._getStaticString("main", "str1"));
+        errorCount += checker("static main.str2", "2", jj._getStaticString("main", "str2"));
 
         assert(errorCount == 0);
     }
