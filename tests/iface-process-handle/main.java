491ce95037eb6e1d3688f260d43d243cbb789b64
Richard Elkins <richard.elkins@gmail.com>
2025-03-13 12:35:58 -0500

    new test case: iface-process-handle

---
+++ tests/iface-process-handle/main.java
@@ -0,0 +1,17 @@
+public class main {
+
+    public static void main(String args[]) {
+
+		ProcessHandle ph = ProcessHandle.current();
+		System.out.println("1. ProcessHandle instantiated");
+		ProcessHandle.Info info = ph.info();
+		System.out.println("2. ProcessHandle.Info instantiated");
+		String jvmPath = info.command().orElse("?");
+		if (jvmPath.equals("?")) {
+			throw new AssertionError("*** ERROR, parent: ProcessHandle.Info.command() failed.");
+		}
+	    System.out.print("3. parent: JVM executable: ");
+	    System.out.println(jvmPath);
+
+    }
+}
