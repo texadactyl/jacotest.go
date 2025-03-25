f537f6b0807dc81252e5121d578d86f99497e225
Richard Elkins <richard.elkins@gmail.com>
2025-03-25 09:22:56 -0500

    Delete obsolete tests.MINI directory

--- tests.MINI/blech-1/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-
-     public static void main(String[] args) {
-        System.out.println("Blech-1! ..... INVALID CATEGORY #1");
-        System.exit(1);
-   }
-
-}
-
--- tests.MINI/blech-2/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-
-     public static void main(String[] args) {
-        System.out.println("Blech-2! ..... INVALID CATEGORY #2");
-        System.exit(1);
-   }
-
-}
-
