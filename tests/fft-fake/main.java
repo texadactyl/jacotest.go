f7f7c04272e94e9ef93de26e12bde14a9b8494f8
Richard Elkins <richard.elkins@gmail.com>
2025-03-27 10:44:45 -0500

    new test case: fft-fake

---
+++ tests/fft-fake/main.java
@@ -0,0 +1,32 @@
+// hacked from scimark2, kernal.java
+
+public class main {
+
+    private static double maxRunTime = 3; // seconds
+
+    public static void main(String[] args) throws InterruptedException {
+
+        long cycles = 1;
+        Stopwatch Q = new Stopwatch();
+        double et; // seconds
+
+        while (true) {
+            Q.resume();
+            for (int ix = 0; ix < cycles; ix++) {
+                Thread.sleep(3);
+            }
+            Q.stop();
+            et = Q.stop();
+            if (et >= maxRunTime)
+                break;
+
+            cycles *= 2;
+        }
+
+        et = Q.read();
+        double cps = cycles / et;
+        System.out.printf("cycles: %d, elapsed time (s): %f, cycles/second: %f\n", cycles, et, cps);
+
+    }
+
+}
