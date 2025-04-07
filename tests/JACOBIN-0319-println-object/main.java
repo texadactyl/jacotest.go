5248e1aa78a8dc12fc3caade0627126cc5c17f25
Richard Elkins <richard.elkins@gmail.com>
2025-04-07 10:23:13 -0500

    JACOBIN-0319-println-object - check for null and empty boundary cases in Object & String

--- tests/JACOBIN-0319-println-object/main.java
+++ tests/JACOBIN-0319-println-object/main.java
@@ -2,7 +2,7 @@ public class main {
 
     public static void printLabeledObject(String label, Object obj) {
         System.out.print(label);
-        System.out.print(": obj=");
+        System.out.print(": ");
         System.out.println(obj);
     }
 
@@ -11,28 +11,39 @@ public class main {
 		
 		String ss = "42";
 		obj = ss;
-		printLabeledObject("String ss: ", obj);
+		printLabeledObject("String ss", obj);
 		
 		int ii = 42;
 		obj = ii;
-		printLabeledObject("int ii: ", obj);
+		printLabeledObject("int ii", obj);
 		
 		long jj = 42l;
 		obj = jj;
-		printLabeledObject("long jj: ", obj);
+		printLabeledObject("long jj", obj);
 		
 		float ff = 42f;
 		obj = ff;
-		printLabeledObject("float ff: ", obj);
+		printLabeledObject("float ff", obj);
 		
 		double dd = 42d;
 		obj = dd;
-		printLabeledObject("double dd: ", obj);
+		printLabeledObject("double dd", obj);
 		
 		byte bb = 0x42;
 		obj = bb;
-		printLabeledObject("byte bb: ", obj);
+		printLabeledObject("byte bb", obj);
 		
+		obj = null;
+		printLabeledObject("Object obj null", obj);
+
+		obj = ss;
+		printLabeledObject("Object obj (=ss)", obj);
+
+		ss = null;
+		printLabeledObject("String ss null", ss);
+
+        ss = "";
+		printLabeledObject("String ss \"\"", ss);
     }
 
 }
