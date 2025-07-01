ae640040719b2a2a2d5c2bd5a946f2505a8b11cc
Richard Elkins <richard.elkins@gmail.com>
2025-07-01 15:33:15 -0500

    correct error reporting in vectors

--- tests/vectors/main.java
+++ tests/vectors/main.java
@@ -6,17 +6,12 @@ import java.util.Vector;
 
 public class main {
 
-    public static void printLabeledString(String label, Object value) {
-        System.out.print(label);
-        System.out.println(value);
-    }
-
-    public static int isItTrue(String label, boolean bool) {
-        if (bool) {
-            printLabeledString("Success :: ", label);
+    public static int isItTrue(String label, int observed, int expected) {
+        if (expected == observed) {
+            System.out.printf("Success :: %s\n", label);
             return 0;
         }
-        printLabeledString("*** ERROR :: ", label);
+        System.out.printf("*** ERROR: %s, expected = %d, observed = %d\n", label, expected, observed);
         return 1;
     }
 
@@ -31,12 +26,12 @@ public class main {
         for (Integer II : vintIntegers) 
             vint.add(II);
         System.out.printf("Vector vint(6) as a String: %s\n", vint.toString());
-        errorCount += isItTrue("vint.capacity() = 10", vint.capacity() == 10);
-        errorCount += isItTrue("vint.size() = 6", vint.size() == 6);
+        errorCount += isItTrue("vint.capacity() = 10", vint.capacity(), 10);
+        errorCount += isItTrue("vint.size() = 6", vint.size(), 6);
 
         // Elongate the vector to 8 elements
         vint.ensureCapacity(10);
-        errorCount += isItTrue("vint.capacity() = 10", vint.capacity() == 10);
+        errorCount += isItTrue("vint.capacity() = 10", vint.capacity(), 10);
         vint.add(7);
         vint.add(8);
         int sum = 0;
@@ -44,7 +39,7 @@ public class main {
             sum += elem;
             System.out.printf(">> elem: %d, sum: %d\n", elem, sum);
         }
-        errorCount += isItTrue("sum of vint elements BEFORE removing '4' = 36", sum == 36);
+        errorCount += isItTrue("sum of vint elements BEFORE removing '4' = 36", sum, 36);
 
         // Remove some elements
         vint.removeElementAt(3);
@@ -53,13 +48,19 @@ public class main {
             sum += elem;
             System.out.printf(">> elem: %d, sum: %d\n", elem, sum);
         }
-        errorCount += isItTrue("sum of vint elements AFTER removing '4' = 32", sum == 32);
+        errorCount += isItTrue("sum of vint elements AFTER removing '4' = 32", sum, 32);
         vint.removeAllElements();
-        errorCount += isItTrue("After removing all elements, vint.size() = 0", vint.size() == 0);
-        errorCount += isItTrue("After removing all elements, vint.isEmpty() is true", vint.isEmpty());
+        errorCount += isItTrue("After removing all elements, vint.size() = 0", vint.size(), 0);
+        
+        if (vint.isEmpty()) {
+            System.out.println("Success :: After removing all elements, vint.isEmpty() is true");
+        } else {
+            errorCount += 1;
+            System.out.println("*** ERROR: After removing all elements, vint.isEmpty() is false");
+        }
 
         // Check the error count
-        System.out.printf("Check assertion using errorCount == %d .....\n", errorCount);
+        System.out.printf("Check assertion using errorCount, %d .....\n", errorCount);
         assert (errorCount == 0);
     }
 }
