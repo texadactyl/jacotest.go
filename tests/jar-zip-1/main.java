a8bc126b1f54454678a2879acfed81c91b49bbff
Richard Elkins <richard.elkins@gmail.com>
2025-11-18 13:06:51 -0600

    giving up on powershell; using 7zip on windows

--- tests/jar-zip-1/main.java
+++ tests/jar-zip-1/main.java
@@ -72,7 +72,8 @@ public class main {
         
         if (os.contains("windows")) {
             System.out.println("\n======================================= main: Create zipB.zip with the powershell utility");
-            execCommand("powershell Compress-Archive -Path middle/calculator2 -DestinationPath zipB.zip");
+            // Richard gives up: execCommand("powershell Compress-Archive -Path middle/calculator2 -DestinationPath zipB.zip");
+            execCommand("7z a zipB.zip middle/calculator2/Calculator2.class");
         } else {
             System.out.println("\n======================================= main: Create zipB.zip with the gnu zip utility");
             execCommand("zip zipB.zip middle/calculator2/Calculator2.class");
@@ -155,3 +156,4 @@ class jj {
     }
    
 }
+
