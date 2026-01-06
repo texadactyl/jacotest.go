ba35ce9a863292e796daec4452d12c3947e1f925
Richard Elkins <richard.elkins@gmail.com>
2026-01-06 15:32:12 -0600

    stop INVOKEINTERFACE in interface-10-Invokeinterface

--- tests/interface-10-Invokeinterface/I08.java
+++ tests/interface-10-Invokeinterface/I08.java
@@ -2,13 +2,15 @@ interface I08 {
     // Private helper method introduced in Java 9 for default method composition
     private String helper(String name) {
         System.out.println("DEBUG entered helper");
-        return "[helper:" + name + "]";
+        //return "[helper:" + name + "]";
+        return String.format("helper: %s", name);
     }
 
     // Default method can call private methods in the same interface
     default String greet(String name) {
         System.out.println("DEBUG entered greet");
-        return "greet: " + name + " " + helper(name);
+        // return "greet: " + name + " " + helper(name);
+        return String.format("greet: %s", helper(name));
     }
 
     // Static interface method – callable only via the interface name
